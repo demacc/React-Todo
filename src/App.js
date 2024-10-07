f8eb10b211414111dfff057e2e7413a3a5221526
Thein Htike Aung <senku9123@gmail.com>
2024-10-07 17:34:43 +0630

    dataFetch

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import TodoList from "./components/TodoList";
 import CheckAllAndRemaining from "./components/CheckAllAndRemaining";
 import FliterTodos from "./components/FliterTodos";
 import ClearupTodos from "./components/ClearupTodos";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/todos")
+      .then((res) => res.json())
+      .then((todos) => setTodos(todos));
+  }, []);
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
         <TodoForm />
-        <TodoList />
+        <TodoList todos={todos} />
         <CheckAllAndRemaining />
         <div className="other-buttons-container">
           <FliterTodos />
