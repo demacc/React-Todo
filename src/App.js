5d9027b82523aa95c28d795c1e4bfc2be015c2dd
Thein Htike Aung <senku9123@gmail.com>
2024-10-07 19:49:19 +0630

    TodoDeleteFeature

--- src/App.js
+++ src/App.js
@@ -14,12 +14,41 @@ function App() {
       .then((res) => res.json())
       .then((todos) => setTodos(todos));
   }, []);
+
+  let addTodo = (todo) => {
+    //serversite data upload
+
+    fetch("http://localhost:3001/todos", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(todo),
+    });
+
+    //clientsite data upload
+    setTodos((preState) => [...preState, todo]);
+  };
+
+  let deleTodo = (todoID) => {
+    // server
+    fetch(`http://localhost:3001/todos/${todoID}`, {
+      method: "DELETE",
+    });
+    // client
+    setTodos((preState) => {
+      return preState.filter((todo) => {
+        return todo.id !== todoID;
+      });
+    });
+  };
   return (
     <div className="todo-app-container">
       <div className="todo-app">
-        <h2>Todo App</h2>
-        <TodoForm />
-        <TodoList todos={todos} />
+        <h2>Todo Web</h2>
+        <TodoForm addTodo={addTodo} />
+        <TodoList todos={todos} deleTodo={deleTodo} />
         <CheckAllAndRemaining />
         <div className="other-buttons-container">
           <FliterTodos />
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function TodoForm() {
+export default function TodoForm({ addTodo }) {
+  let [title, setTitle] = useState("");
+  let submitHandler = (e) => {
+    e.preventDefault();
+    let todo = {
+      id: Math.random(),
+      title,
+      completed: false,
+    };
+    //addtodo
+    addTodo(todo);
+    setTitle("");
+  };
   return (
-    <form action="#">
+    <form action="#" onSubmit={submitHandler}>
       <input
         type="text"
         className="todo-input"
         placeholder="What do you need to do?"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
       />
     </form>
   );
